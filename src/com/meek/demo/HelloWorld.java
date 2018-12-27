75b33d920e7825f83f8f2a3d331f6224888c473f
ponyo <appiumscript@163.com>
2018-12-27 14:59:04 +0800

    Initial commit

---
+++ src/com/meek/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.meek.demo;
+
+/**
+ * Comment:仉乾真 2018年12月27日
+ * 练习使用IDEA。
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
+}
